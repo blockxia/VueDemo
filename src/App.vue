b964497dfea0bf5db1fa59770a3f95d409fe11e2
xiaoxia <920634755@qq.com>
2018-07-01 00:17:55 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <TodoHeader :addTodo="addTodo"/>
+      <List :todos="todos" :deleteTodo="deleteTodo"/>
+      <TodoFooter :todos="todos" :deleteCompleted="deleteCompleted" :selectAll="selectAll"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import Header from './components/header.vue'
+  import List from './components/list.vue'
+  import Footer from './components/footer.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    data(){
+      return{
+        todos:[
+          {title: '吃饭', completed: false},
+          {title: '睡觉', completed: true},
+          {title: '打代码', completed: false}
+        ]
+      }
+    },
+     components:{
+       TodoHeader:Header,
+       List,
+       TodoFooter:Footer
+     },
+    methods:{
+      addTodo (todo) {
+        this.todos.unshift(todo)
+      },
+      deleteTodo (index) {
+        this.todos.splice(index,1)
+      },
+      deleteCompleted(){
+        this.todos=this.todos.filter(todo=>!todo.completed)
+
+      },//全选或全不选
+      selectAll(check){
+        this.todos.forEach(todo=>todo.completed=check)
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
+  /*app*/
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+
 </style>
